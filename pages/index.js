95388a9fbd9bddab3fe63c5c79ee378994f4bf1e
datagutt <datagutt@lekanger.no>
2019-12-26 14:37:15 +0100

    Test Pulse

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,15 @@
+import Pulse from 'pulse-framework/dist/pulse.min';
+import React, { useState } from 'react';
 import Nav from '../components/Nav'
+import core from '../lib/core';
 
-const IndexPage = () => (
-  <>
-    <Nav />
-    <p>Hello, I'm the index page</p>
-  </>
-)
+const IndexPage = Pulse.React(() => {
+  return (
+    <>
+      <Nav />
+      <p>Hello, I'm the index page</p>
+    </>
+  );
+});
 
 export default IndexPage
