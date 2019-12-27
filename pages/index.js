be16ee3a0af05a178239b6ad6d42d5484a1e09e8
datagutt <datagutt@lekanger.no>
2019-12-27 17:06:39 +0100

    I mean, it works client side atleast

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,31 @@
 import Pulse from 'pulse-framework/dist/pulse.min';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Nav from '../components/Nav'
 import core from '../lib/core';
 
 const IndexPage = Pulse.React(() => {
+  useEffect(() => {
+    core.posts.getPosts();
+  }, [])
+  if(!core.posts.homepage) return null;
   return (
     <>
       <Nav />
       <p>Hello, I'm the index page</p>
+      {
+        core.posts.homepage
+        &&
+        core.posts.homepage.map((post, i) => {
+          return (
+            <div key={i}>
+              <h1>{post.title}</h1>
+              <p>{post.body}</p>
+            </div>
+          )
+        })
+      }
     </>
   );
-});
+}, (core) => [core.posts.homepage]);
 
 export default IndexPage
